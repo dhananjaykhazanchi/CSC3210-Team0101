5be14d154bf5f66ad6a799a0a1c92446e91546f3
Thu Vo <tvo55@student.gsu.edu>
2020-02-18 20:44:42 -0500

    upload project 2 codes and folder

---
+++ Assignment 2/Code/Thu/arithmetic2_thu.s
@@ -0,0 +1,20 @@
+.section .data
+val1: .word 6
+val2: .word 11
+val3: .word 16
+.section .text
+.globl _start
+_start:
+ldr r1, =val2
+ldr r1, [r1]
+add r1, #9  
+ldr r2, =val3
+ldr r2, [r2] 
+add r1, r1, r2   
+ldr r2, =val1
+ldr r2, [r2]
+sub r1, r2
+
+mov r7, #1 
+svc #0 
+.end
