ae0117e8e611c2fa7b3ca9776b11366c38f2e88d
Thu Vo <tvo55@student.gsu.edu>
2020-03-05 20:38:39 -0500

    screenshot added

--- Assignment3/Code/Thu/reduction.c
+++ Assignment3/Code/Thu/reduction.c
@@ -36,7 +36,7 @@ return sum;
 int parallelSum(int* a, int n) {
 int sum = 0;
  int i;
-#pragma omp parallel for // reduction(+:sum)
+#pragma omp parallel for  reduction(+:sum)
  for (i = 0; i < n; i++) {
 sum += a[i];
 }
